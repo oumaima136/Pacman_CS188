a8353a7b4f4d6f8ac3f5de5faabc746b57c8f269
Oumaima Benatia <oumaimabenatia24@gmail.com>
2023-08-10 16:14:19 +0100

    commit 6

--- graphicsUtils.py
+++ graphicsUtils.py
@@ -7,7 +7,7 @@
 # 
 #  Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan in (klein@cs.berkeley.edu).
+# (denero@cs.berkeley.edu) and Dan(klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
 
