4ed8cee1824f3df0ea485741a6a784b3cd00409c
Oumaima Benatia <oumaimabenatia24@gmail.com>
2023-08-10 16:19:39 +0100

    commit 14

--- graphicsUtils.py
+++ graphicsUtils.py
@@ -1,19 +1,23 @@
 # graphicsUtils.py
 # ----------------
-#extend these projects for
+# Licensing Information:  You are free to use or extend these projects for
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
 # 
-#  Information: The Pacman AI projects were developed at UC Berkeley.
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan(klein@cs.berkeley.edu).
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
-#jowshncolaxiji wd
+
 
 import sys
+import math
+import random
+import string
 import time
+import types
 import Tkinter
 import os.path
 
